01190b25a6d90d32e9b9ef163a2f0a305c2b7167
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-03-08 22:57:42 -0800

    twitter, spotify, and movie working

--- liri.js
+++ liri.js
@@ -9,16 +9,16 @@ var client = new Twitter(keys.twitter);
 
 
 var command = process.argv[2];
-var search = process.argv[3] 
+var query = process.argv[3] 
 
 for (var i = 4; i < process.argv.length; i++) {
-        search += "+" +process.argv[i]; 
+        query += "+" +process.argv[i]; 
     }
 
 //---TWITTER
 if (command === "my-tweets") {
-    var params = {screen_name: 'freddegurre'};
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    var user = {screen_name: 'freddegurre'};
+    client.get('statuses/user_timeline', user, function(error, tweets, response) {
       if (!error) {
           for (var i = 0; i < tweets.length; i++) {
             console.log(tweets[i].text + " | tweeted on | " + tweets[i].created_at); 
@@ -27,29 +27,33 @@ if (command === "my-tweets") {
         }
     });
 
-
 }
 //-----SPOTIFY
 if (command === "spotify-this-song") {
-    
-    spotify.search ({ type: "track", search: song, limit: 1}, function(error, data){
+    spotify.search({ type: "track", query: query, limit: 1}, function(error, data){
         if (error) {console.log(error);}
-
-        else if (song === "") {
-            song = "The sign"
-        }
-        console.log(data.tracks.items); 
         
-    })
-   
+        console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+        console.log("Songs Name: " + data.tracks.items[0].name);
+        console.log("Song URL : " + data.tracks.items[0].external_urls.spotify);
+        console.log("Album : " + data.tracks.items[0].album.name);
+    }); 
 }
 //-----Movie
 if (command === "movie-this") {
     var apiKey = "trilogy"
-    var movieURL = "http://www.omdbapi.com/?apikey="+ apiKey+ "&s=batman"
+    var movieURL = "http://www.omdbapi.com/?apikey="+ apiKey+ "&t=" + query
     request(movieURL, function(error, response, body){
         if(error) {console.log("you have error" + error)}
-        console.log(body); 
-        
+        var movie = JSON.parse(body); 
+        console.log("Title: " + movie.Title);    
+        console.log("Released: " + movie.Released);
+        console.log("IMDB:  " + movie.imdbRating);
+        console.log("Rotten Tomatoes: " + movie.Ratings[1].Value);
+        console.log("Produced:  " +  movie.Country);
+        console.log("Language:  " +  movie.Language);
+        console.log("Actors:  " +  movie.Actors);
+        console.log("Plot:  " +  movie.Plot);
     })
 }
+
